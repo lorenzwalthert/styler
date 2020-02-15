82ec497739a0c67d769261ad0bb1ebb31edb38e8
Lorenz Walthert <lorenz.walthert@icloud.com>
2020-02-15 11:46:57 +0100

    better example

--- tests/testthat/test-interaction-caching-comments.R
+++ tests/testthat/test-interaction-caching-comments.R
@@ -1,5 +1,4 @@
-cache_clear(ask = FALSE)
-cache_activate()
+fresh_testthat_cache()
 # Using cache 1.3.1.9000 at /Users/lorenz/Library/Caches/R/R.cache/styler/1.3.1.9000.
 text7 <- c(
   "call(",
@@ -11,7 +10,7 @@ text7 <- c(
 )
 style_text(text7)
 
-text7 <- c(
+text8 <- c(
   "call(",
   "# styler: off",
   "1 +1,",
@@ -23,4 +22,18 @@ text7 <- c(
 # comment in a function call, will be preserved since with drop_cached_chidlren(),
 # all comments are preserved (but we should only preserve top-level comments).
 
-style_text(text7)
+style_text(text8)
+
+# however, for some reason, we cannot isolate the problem when stylerignore is
+# not used. It appears as then, the call is not cached properly
+
+# here is a smaller reprex
+fresh_testthat_cache()
+compute_parse_data_nested(text7, tidyverse_style())
+
+cache_deactivate()
+
+text7_styled <- as.character(style_text(text7))
+activate_testthat_cache()
+cache_by_expression(text7, tidyverse_style())
+compute_parse_data_nested(text8, tidyverse_style())
